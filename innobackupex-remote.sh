a6f06ca767d73f045e41c23658c544bccf855fc7
Jonathan Petersson <jpetersson@redbridge.se>
2019-12-08 08:20:45 +0100

    Removed host-folder validation as this is redundant, full backup will run if there isn't any previous backups available, further compressed incr definition and only specify one call

--- innobackupex-remote.sh
+++ innobackupex-remote.sh
@@ -6,10 +6,6 @@ today=$(date +%Y-%m-%d_%H)
 h_path="/backup/xtrabackup/${host}"
 b_path="${h_path}/${today}"
 
-if [[ ! -d ${h_path} ]]; then
-    level="Full"
-fi
-
 if [[ ! -d ${b_path} ]]; then
     mkdir -p ${b_path}
 else
@@ -17,13 +13,12 @@ else
     exit 1
 fi
 
-exec &>> ${b_path}/xtrabackup.log
-
 latest=$(ls -td $h_path/* -- 2> /dev/null | sed -n '2p')
 lsn=$(grep -Po "^to_lsn = \K.*" ${latest}/xtrabackup_checkpoints)
 
 if [[ "$level" == "Incremental" && -n $lsn ]]; then
-    ssh mysql@${host} " --compress --slave-info --safe-slave-backup --no-timestamp --stream=xbstream --incremental /tmp/${today}_mysqlxtrabackup/ --incremental-lsn=${lsn}" | xbstream -x -C ${b_path}/
-else
-    ssh mysql@${host} " --compress --slave-info --safe-slave-backup --no-timestamp --stream=xbstream /tmp/${today}_mysqlxtrabackup" | xbstream -x -C ${b_path}/
+    incremental="--incremental --incremental-lsn=${lsn}"
 fi
+
+exec &>> ${b_path}/xtrabackup.log
+ssh mysql@${host} " --compress --slave-info --safe-slave-backup --no-timestamp --stream=xbstream ${incremental} /tmp/${today}_mysqlxtrabackup" | xbstream -x -C ${b_path}/
